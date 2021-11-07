90f70cec6fbe44b544f8b9326e7bf840263135a4
djjin <wlseowhd12@naver.com>
2021-11-07 22:26:27 +0900

    modified urdf

--- example/collision/all_collision_test.py
+++ example/collision/all_collision_test.py
@@ -12,6 +12,7 @@ from pykin.kinematics.transform import Transform
 from pykin.collision.collision_manager import CollisionManager
 from pykin.utils.collision_utils import apply_robot_to_collision_manager, apply_robot_to_scene
 
+# iiwa
 file_path = '../../asset/urdf/iiwa14/iiwa14.urdf'
 robot = SingleArm(file_path, Transform(rot=[0.0, 0.0, np.pi/2], pos=[0, 0, 0]))
 
@@ -19,8 +20,6 @@ custom_fpath = '../../asset/config/iiwa14_init_params.json'
 with open(custom_fpath) as f:
             controller_config = json.load(f)
 init_qpos = controller_config["init_qpos"]
-
-print(init_qpos)
 
 fk = robot.forward_kin(np.array(init_qpos))
 mesh_path = pykin_path+"/asset/urdf/iiwa14/"
@@ -34,9 +33,9 @@ scene = trimesh.Scene()
 scene = apply_robot_to_scene(scene=scene, mesh_path=mesh_path, robot=robot, fk=fk)
 scene.set_camera(np.array([np.pi/2, 0, np.pi/2]), 5, resolution=(1024, 512))
 
+# panda
 file_path = '../../asset/urdf/panda/panda.urdf'
 robot = SingleArm(file_path, Transform(rot=[0.0, 0.0, np.pi/2], pos=[1, 0, 0]))
-
 custom_fpath = '../../asset/config/panda_init_params.json'
 with open(custom_fpath) as f:
             controller_config = json.load(f)
@@ -50,46 +49,39 @@ c_manager = apply_robot_to_collision_manager(c_manager, robot, fk)
 
 result, objs_in_collision, contact_data = c_manager.collision_check(return_names=True, return_data=True)
 print(result, objs_in_collision, len(contact_data))
-
-# scene = trimesh.Scene()
 scene = apply_robot_to_scene(scene=scene, mesh_path=mesh_path, robot=robot, fk=fk)
+
+# sawyer
 custom_fpath = '../../asset/config/sawyer_init_params.json'
 with open(custom_fpath) as f:
             controller_config = json.load(f)
 init_qpos = controller_config["init_qpos"]
 
 file_path = '../../asset/urdf/sawyer/sawyer.urdf'
-
 robot = SingleArm(file_path, Transform(rot=[0.0, 0.0, np.pi/2], pos=[2, 0, 0]))
-
-# fk = robot.forward_kin(np.array([0, np.pi/2, np.pi/2, np.pi/3, -np.pi/2, -np.pi/2, -np.pi/2, np.pi/2]))
 fk = robot.forward_kin(np.array(np.concatenate((np.zeros(1), init_qpos))))
-# fk = robot.forward_kin(np.zeros(8))
-mesh_path = pykin_path+"/asset/urdf/sawyer/"
 
-# init_trainsform
-# 로봇의 경우 첫 collision은 제외 일어난 이름은 제외 
+mesh_path = pykin_path+"/asset/urdf/sawyer/"
 c_manager = CollisionManager(mesh_path)
 c_manager.filter_contact_names(robot)
 c_manager = apply_robot_to_collision_manager(c_manager, robot, fk)
 test, name, data = c_manager.collision_check(return_names=True, return_data=True)
-
-# scene = trimesh.Scene()
 scene = apply_robot_to_scene(scene=scene, mesh_path=mesh_path, robot=robot, fk=fk)
 
+# baxter
+file_path = '../../asset/urdf/baxter/baxter.urdf'
+robot = Bimanual(file_path, Transform(rot=[0.0, 0.0, np.pi/2], pos=[-1, 0, 0]))
 
-# milk_path = pykin_path+"/asset/objects/meshes/milk.stl"
-# test_mesh = trimesh.load_mesh(milk_path)
-# scene.add_geometry(test_mesh, node_name="milk1", transform=Transform(pos=[0.1, 0, 0]).h_mat)
-# scene.add_geometry(test_mesh, node_name="milk2", transform=Transform(pos=[0.1, 0, 0.1]).h_mat)
+custom_fpath = '../../asset/config/baxter_init_params.json'
+with open(custom_fpath) as f:
+            controller_config = json.load(f)
+init_qpos = controller_config["init_qpos"]
+fk = robot.forward_kin(np.concatenate((np.zeros(1), np.array(init_qpos))))
 
-# o_manager = CollisionManager(milk_path)
-# o_manager.add_object("milk1", gtype="mesh", gparam=test_mesh, transform=Transform(pos=[0.1, 0, 0]).h_mat)
-# o_manager.add_object("milk2", gtype="mesh", gparam=test_mesh, transform=Transform(pos=[0.1, 0, 0.1]).h_mat)
-# test, name, data = o_manager.collision_check(return_names=True, return_data=True)
-# print(test, name)
-
-# test, name, data = c_manager.collision_check(o_manager, True, True)
-# print(test, name)
+mesh_path = pykin_path+"/asset/urdf/baxter/"
+c_manager = CollisionManager(mesh_path)
+c_manager.filter_contact_names(robot, fk)
+c_manager = apply_robot_to_collision_manager(c_manager, robot, fk)
+scene = apply_robot_to_scene(scene=scene, mesh_path=mesh_path, robot=robot, fk=fk)
 
 scene.show()
