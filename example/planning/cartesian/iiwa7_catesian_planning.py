a513c53a995fadf7324b17bd9c35e102f2b2cff7
djjin <wlseowhd12@naver.com>
2021-11-19 16:07:51 +0900

    add iiwa7

---
+++ example/planning/cartesian/iiwa7_catesian_planning.py
@@ -0,0 +1,97 @@
+import numpy as np
+import argparse
+import sys, os
+import json
+
+pykin_path = os.path.abspath(os.path.dirname(__file__)+"../../../" )
+sys.path.append(pykin_path)
+
+from pykin.robots.single_arm import SingleArm
+from pykin.kinematics.transform import Transform
+from pykin.utils import plot_utils as plt
+from pykin.planners.cartesian_planner import CartesianPlanner
+from pykin.collision.collision_manager import CollisionManager
+from pykin.utils.collision_utils import apply_robot_to_collision_manager, apply_robot_to_scene
+
+
+help_str = "python iiwa7_cartesian_planning.py"\
+            " --timesteps 500 --damping 0.03 --resolution 0.2 --pos-sensitivity 0.03"
+
+parser = argparse.ArgumentParser(usage=help_str)
+parser.add_argument("--timesteps", type=int, default=500)
+parser.add_argument("--damping", type=float, default=0.03)
+parser.add_argument("--resolution", type=float, default=0.2)
+parser.add_argument("--pos-sensitivity", type=float, default=0.05)
+args = parser.parse_args()
+
+file_path = '../../../asset/urdf/iiwa7/iiwa7.urdf'
+mesh_path = pykin_path+"/asset/urdf/iiwa7/"
+json_path = '../../../asset/config/iiwa14_init_params.json'
+
+with open(json_path) as f:
+            controller_config = json.load(f)
+init_qpos = controller_config["init_qpos"]
+
+robot = SingleArm(file_path, Transform(rot=[0.0, 0.0, 0], pos=[0, 0, 0]))
+robot.setup_link_name("iiwa7_link_0", "iiwa7_link_ee")
+
+##################################################################
+init_fk = robot.forward_kin(init_qpos)
+
+# target_joints = [0.0, 1.2, 0.0, -1.89, 0.0, 0.6, 0.0]
+# robot.offset.pos = [1, 0, 0]
+# goal_transformations = robot.forward_kin(target_joints)
+
+# scene = trimesh.Scene()
+# scene = apply_robot_to_scene(scene=scene, mesh_path=mesh_path, robot=robot, fk=init_fk)
+# scene = apply_robot_to_scene(scene=scene, mesh_path=mesh_path, robot=robot, fk=goal_transformations)
+# scene.set_camera(np.array([np.pi/2, 0, np.pi/2]), 5, resolution=(1024, 512))
+
+# scene.show()
+
+init_eef_pose = robot.get_eef_pose(init_fk)
+# goal_eef_pose = robot.get_eef_pose(goal_transformations)
+goal_eef_pose = controller_config["goal_pos"]
+##################################################################
+
+c_manager = CollisionManager(mesh_path)
+c_manager.filter_contact_names(robot, init_fk)
+c_manager = apply_robot_to_collision_manager(c_manager, robot, init_fk)
+
+task_plan = CartesianPlanner(
+    robot, 
+    self_collision_manager=c_manager,
+    obstacle_collision_manager=None,
+    n_step=args.timesteps,
+    dimension=7)
+
+joint_path, target_poses = task_plan.get_path_in_joinst_space(
+    current_q=init_qpos,
+    goal_pose=goal_eef_pose,
+    resolution=args.resolution, 
+    damping=args.damping,
+    pos_sensitivity=args.pos_sensitivity)
+
+if joint_path is None and target_poses is None:
+    print("Cannot Visulization Path")
+    exit()
+
+joint_trajectory = []
+for joint in joint_path:
+    transformations = robot.forward_kin(joint)
+    joint_trajectory.append(transformations)
+
+print(f"Computed Goal Position : {joint_trajectory[-1][robot.eef_name].pose}")
+print(f"Desired Goal position : {target_poses[-1]}")
+
+fig, ax = plt.init_3d_figure(figsize=(10,6), dpi= 100)
+
+plt.plot_animation(
+    robot,
+    joint_trajectory,
+    fig=fig, 
+    ax=ax,
+    visible_collision=True,
+    eef_poses=target_poses,
+    obstacles=[],
+    repeat=True)
---
+++ example/planning/rrt_star/iiwa7_rrt_star_planning.py
@@ -0,0 +1,97 @@
+import numpy as np
+import sys, os
+import json
+import trimesh
+
+pykin_path = os.path.abspath(os.path.dirname(__file__)+"../../../" )
+sys.path.append(pykin_path)
+
+from pykin.robots.single_arm import SingleArm
+from pykin.planners.rrt_star_planner import RRTStarPlanner
+from pykin.collision.collision_manager import CollisionManager
+from pykin.kinematics.transform import Transform
+from pykin.utils.obstacle_utils import Obstacle
+from pykin.utils import plot_utils as plt
+from pykin.utils.collision_utils import apply_robot_to_collision_manager, apply_robot_to_scene
+
+fig, ax = plt.init_3d_figure(figsize=(10,6), dpi= 100)
+
+file_path = '../../../asset/urdf/iiwa7/iiwa7.urdf'
+mesh_path = pykin_path+"/asset/urdf/iiwa7/"
+json_path = '../../../asset/config/iiwa14_init_params.json'
+
+with open(json_path) as f:
+            controller_config = json.load(f)
+
+robot = SingleArm(file_path, Transform(rot=[0.0, 0.0, 0], pos=[0, 0, 0]))
+robot.setup_link_name("iiwa7_link_0", "iiwa7_link_ee")
+
+##################################################################
+init_qpos = controller_config["init_qpos"]
+init_fk = robot.forward_kin(init_qpos)
+init_eef_pose = robot.get_eef_pose(init_fk)
+goal_eef_pose = controller_config["goal_pose"]
+
+c_manager = CollisionManager(mesh_path)
+c_manager.filter_contact_names(robot, init_fk)
+c_manager = apply_robot_to_collision_manager(c_manager, robot, init_fk)
+
+milk_path = pykin_path+"/asset/objects/meshes/milk.stl"
+milk_mesh = trimesh.load_mesh(milk_path)
+
+obs = Obstacle()
+o_manager = CollisionManager(milk_path)
+for i in range(9):
+    name = "miik_" + str(i)
+    if i < 3:
+        obs_pos = [0.3, -0.5 + i * 0.5, 0.3] 
+    elif 3 <= i < 6:
+        obs_pos = [0.3, -0.5 + (i-3) * 0.5, 0.9] 
+    else:
+        obs_pos = [0.3, -0.5 + (i-6) * 0.5, -0.3]
+
+    o_manager.add_object(name, gtype="mesh", gparam=milk_mesh, transform=Transform(pos=obs_pos).h_mat)
+    obs(name=name, gtype="mesh", gparam=milk_mesh, gpose=Transform(pos=obs_pos))
+
+##################################################################
+
+planner = RRTStarPlanner(
+    robot=robot,
+    self_collision_manager=c_manager,
+    obstacle_collision_manager=o_manager,
+    delta_distance=0.05,
+    epsilon=0.2, 
+    max_iter=1500,
+    gamma_RRT_star=1,
+    dimension=7
+)
+
+joint_path = planner.get_path_in_joinst_space(cur_q=init_qpos, goal_pose=goal_eef_pose)
+
+if joint_path is None :
+    print("Cannot Visulization Path")
+    exit()
+
+joint_trajectory = []
+eef_poses = []
+resolution = 0.5
+print(len(joint_path))
+for step, joint in enumerate(joint_path):
+    if step % (1/resolution) == 0 or step == len(joint_path)-1:
+        transformations = robot.forward_kin(joint)
+        joint_trajectory.append(transformations)
+        eef_poses.append(transformations[robot.eef_name].pos)
+
+plt.plot_animation(
+    robot,
+    joint_trajectory, 
+    fig, 
+    ax,
+    eef_poses=eef_poses,
+    obstacles=obs,
+    visible_obstacles=True,
+    visible_collision=True, 
+    interval=1, 
+    repeat=True,
+    result=None)
+
