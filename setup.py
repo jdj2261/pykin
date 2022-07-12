a3ba86d71d65b9f519208c3735da05ece226d752
jdj2261 <wlseowhd12@naver.com>
2022-07-12 23:43:39 +0900

    updated setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "numpy",
         "matplotlib",
         "trimesh[easy]",
-        "pyglet"
+        "pyglet",
         "tqdm",
         "pyyaml",
         "python-fcl",
@@ -31,7 +31,7 @@ setup(
     url="https://github.com/jdj2261/pykin.git",
 	download_url="https://github.com/jdj2261/pykin/archive/refs/heads/main.zip",
     author_email="wlseoeo@gmail.com",
-    version="1.5.0",
+    version="1.5.1",
     long_description=long_description,
     long_description_content_type='text/markdown'
 )
