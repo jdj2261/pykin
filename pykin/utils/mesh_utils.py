9f58a773e92eafc1a2d19c523e18a6b083cf4600
jdj2261 <wlseowhd12@naver.com>
2022-07-18 23:56:14 +0900

    added object meshes

--- pykin/utils/mesh_utils.py
+++ pykin/utils/mesh_utils.py
@@ -17,7 +17,7 @@ def get_mesh_path(mesh_path, robot_name):
 
 def get_object_mesh(mesh_name, scale=[1.0, 1.0, 1.0]):
     file_path = pykin_path + '/asset/objects/meshes/'
-    mesh = trimesh.load(file_path + mesh_name)
+    mesh:trimesh.Trimesh = trimesh.load(file_path + mesh_name)
     mesh.apply_scale(scale)
     return mesh
 
