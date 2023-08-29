5179d08fb0035b1de84b05a1bd5ed256733319a7
jdj2261 <wlseowhd12@naver.com>
2023-08-29 16:49:20 +0900

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-print(find_packages())
 # read the contents of your README file
 from os import path
+import io
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+with io.open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     lines = f.readlines()
 
 # remove images from README
@@ -13,29 +13,27 @@ lines = [x for x in lines if ".png" not in x]
 long_description = "".join(lines)
 
 setup(
-    name="pytamp",
+    name="pykin",
     packages=find_packages(exclude=["tests"]),
     install_requires=[
         "numpy",
         "matplotlib",
         "trimesh[easy]",
+        "pyglet",
         "tqdm",
         "pyyaml",
         "python-fcl",
-        "networkx",
-        "pygraphviz",
-        "opencv-python",
-        "natsort",
+        "Pillow",
     ],
     eager_resources=["*"],
     include_package_data=True,
     python_requires=">=3",
-    description="Robotics Task and Motion Planning Library",
-    author="Daejong Jin",
-    url="https://github.com/jdj2261/pytamp.git",
-    download_url="https://github.com/jdj2261/pytamp/archive/refs/heads/main.zip",
+    description="Robotics Kinematics Library",
+    author="Daejong Jin, Youngtaek Hong, Juhan Park",
+    url="https://github.com/jdj2261/pykin.git",
+    download_url="https://github.com/jdj2261/pykin/archive/refs/heads/main.zip",
     author_email="wlseoeo@gmail.com",
-    version="0.5.1",
+    version="1.6.0" ,
     long_description=long_description,
     long_description_content_type="text/markdown",
 )
